1cb77361aaa5875200b1e49bf110b05b0ed37267
Diego Rubio Romero <dyegoruvyo@gmail.com>
2017-08-13 20:35:01 -0500

    Mi primer archivo hecho en Python !!

---
+++ holamundo.py
@@ -0,0 +1,13 @@
+print("A continuacion hare varios ejemplos en mi primera experiencia con Python")
+print("Hola Mundo :v")
+print("Eso nunca falta xd")
+
+print("¿2+2=3?")
+if 2+2==3:
+	print("Es 4")
+else:
+	print("Illuminati confirmed !!! :v")
+	
+print("Por ultimo hare uso de for para encontrar la suma de numeros consecutivos en su posicion n, a partir de n=1")
+for i in range(1,16):
+    print((i**2)/2 + i/2)
