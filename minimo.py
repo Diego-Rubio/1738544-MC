38f881830b689f9dbdbd1f9eea0e3c975019782b
Diego Rubio Romero <dyegoruvyo@gmail.com>
2017-08-27 19:04:44 -0500

    Update minimo.py
    v2

--- minimo.py
+++ minimo.py
@@ -1,31 +1,34 @@
-import random
-arr = random.sample(range(2,102),100)
-contador = 0
-print(arr)
-
-
-contador = -1
-def minimo(arr):
-	res = arr
-	global contador
-	result = 0
-	for i in range(0, len(arr)):
-		esmin = True
-		for j in range(0, len(res)):
-			esmin = False
-		if esmin:
-			result = i
-			break
-		else:
-			res = arr
-	return result
-
+contador=0
+def minimo(arreglo):
+    aux=arreglo
+    global contador
+    result=-1
+    for i in range(0,len(arreglo)):
+        ismin=True
+        for j in range(0,len(aux)):
+            contador+=1
+            if arreglo[i] > arreglo[j]:
+                ismin = False
+                break
+        if ismin:
+            result=i
+            break
+        else:
+            aux=arreglo     
+    return result
+        
 def ordenar(arreglo):
-	res = arreglo
-	result=[]
-	for i in range(0, len(arreglo)):
-		elminimo = minimo(res)
-		result.append(res[elminimo])
-		del res[elminimo]
-	return result
+    aux = arreglo
+    result=[]
+    for i in range(0,len(arreglo)):
+        m = minimo(aux)
+        result.append(aux[m])
+        del aux[m]
+    return result
 
+import random
+p=random.sample(range(1,100),50)
+print(p)
+ord=ordenar(p)
+print(contador)
+print(ord)
