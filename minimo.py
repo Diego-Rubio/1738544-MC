4262243821826afec0a1ac424be728ba2e04e000
Diego Rubio Romero <dyegoruvyo@gmail.com>
2017-08-29 19:38:18 -0500

    Update minimo.py
    v3

--- minimo.py
+++ minimo.py
@@ -1,21 +1,12 @@
 contador=0
-def minimo(arreglo):
-    aux=arreglo
+def minimo(arr):
+    m = arr[0]
     global contador
-    result=-1
-    for i in range(0,len(arreglo)):
-        ismin=True
-        for j in range(0,len(aux)):
-            contador+=1
-            if arreglo[i] > arreglo[j]:
-                ismin = False
-                break
-        if ismin:
-            result=i
-            break
-        else:
-            aux=arreglo     
-    return result
+    for i in arr:
+        contador+=1
+        if i < m:
+            m=i
+    return m
         
 def ordenar(arreglo):
     aux = arreglo
@@ -32,3 +23,4 @@ print(p)
 ord=ordenar(p)
 print(contador)
 print(ord)
+print(minimo(p))
