8fce071cb5b2e13c7ad1295e3298929924fed26e
Diego Rubio Romero <dyegoruvyo@gmail.com>
2017-10-07 03:42:29 -0500

    Add files via upload

---
+++ Estructura de Datos.py
@@ -0,0 +1,97 @@
+class Pila:
+    def __init__(self):
+        self.pila=[]
+    def obtener(self):
+        return self.pila.pop()
+    def meter(self,e):
+        self.pila.append(e)
+        return len(self.pila)
+    @property
+    def longitud(self):
+        return len(self.pila)
+
+class Cola:
+    def __init__(self):
+        self.cola=[]
+    def obtener(self):
+        return self.cola.pop(0)
+    def meter(self,e):
+        self.cola.append(e)
+        return len(self.cola)
+    @property
+    def longitud(self):
+        return len(self.cola)        
+
+
+class Grafo:
+ 
+    def __init__(self):
+        self.V = set() 
+        self.E = dict() 
+        self.vecinos = dict() 
+ 
+    def agrega(self, v):
+        self.V.add(v)
+        if not v in self.vecinos: 
+            self.vecinos[v] = set() 
+ 
+    def conecta(self, v, u, peso=1):
+        self.agrega(v)
+        self.agrega(u)
+        self.E[(v, u)] = self.E[(u, v)] = peso 
+        self.vecinos[v].add(u)
+        self.vecinos[u].add(v)
+ 
+    def complemento(self):
+        comp= Grafo()
+        for v in self.V:
+            for w in self.V:
+                if v != w and (v, w) not in self.E:
+                    comp.conecta(v, w, 1)
+        return comp        
+ 
+ 
+def dfs(grafo,inicio):
+    visitados=[]
+    pila=Pila()
+    pila.meter(inicio)
+    while pila.longitud>0:
+        actual=pila.obtener()
+        if actual in visitados:
+            continue
+            
+        visitados.append(actual)
+        vecinos=grafo.vecinos[actual]
+        for i in vecinos:
+            if i not in visitados:
+                pila.meter(i)
+    return visitados
+
+def bfs(grafo,inicio):
+    visitados=[inicio]
+    cola=Cola()
+    cola.meter(inicio)
+    while cola.longitud>0:
+        actual=cola.obtener()
+        vecinos=grafo.vecinos[actual]
+        for i in vecinos:
+            if i not in visitados:
+                visitados.append(i)
+                cola.meter(i)
+    return visitados
+ 
+print('El grafo es: {(1,2),(1,3),(2,4),(2,5),(3,6),(4,7),(5,7),(6,8),(6,9)}')
+grafo=Grafo()
+grafo.conecta(1,2)
+grafo.conecta(1,3)
+grafo.conecta(2,4)
+grafo.conecta(2,5)
+grafo.conecta(3,6)
+grafo.conecta(4,7)
+grafo.conecta(5,7)
+grafo.conecta(6,8)
+grafo.conecta(6,9)
+print('Usando DFS:')
+print(dfs(grafo,1))
+print('Usando BFS:')
+print(bfs(grafo,1))
