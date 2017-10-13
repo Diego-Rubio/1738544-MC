18b6500fe8dacaf64502e098794fb9a6b69e9da3
Diego Rubio Romero <dyegoruvyo@gmail.com>
2017-10-13 17:14:45 -0500

    Add files via upload

---
+++ Algoritmos de Fibonacci y Numeros Primos/FibonacciRecursivo.py
@@ -0,0 +1,12 @@
+cnt=0
+def fibonacci(n):
+	global cnt
+	cnt+=1
+	if n==0 or n==1:
+		return 1
+	return fibonacci(n-2) + fibonacci(n-1)
+
+print("Lo siguiente representa:\n1er Columna: Posicion Fibonacci \n2da Columna: Numero de Fibonacci  \n3er Columna: Total de operaciones")
+for i in range(0,51):
+	cnt=0
+	print(i, fibonacci(i), cnt)
